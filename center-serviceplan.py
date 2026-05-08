41ca41361c4b0a704defa3abb19092a21608380a
cubiiz1666 <136550381+cubiiz1666@users.noreply.github.com>
2026-05-08 10:27:44 +0700

    center-serviceplan.py

--- center-serviceplan.py
+++ center-serviceplan.py
@@ -72,8 +72,8 @@ try:
                     xanchor="center",
                     x=0.5                 # จัดให้อยู่กึ่งกลาง
                 ),
-                margin=dict(t=0, b=80, l=0, r=0), # เพิ่ม Margin ด้านล่าง (b) เพื่อไม่ให้โดนตัดขอบ
-                height=300 # เพิ่มความสูงเล็กน้อยเพื่อให้มีพื้นที่สำหรับ Legend
+                margin=dict(t=0, b=50, l=0, r=0), # เพิ่ม Margin ด้านล่าง (b) เพื่อไม่ให้โดนตัดขอบ
+                height=320 # เพิ่มความสูงเล็กน้อยเพื่อให้มีพื้นที่สำหรับ Legend
             )
             st.plotly_chart(fig_donut, use_container_width=True)
 
